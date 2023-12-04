a76f856d082a12740963ee1721d1863f3e206aa7
Yakov Kondratev <kondratev@nerve.ai>
2023-12-04 16:54:26 +0300

    logo import fixed 4

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   return (
     <div className="App">
         <h1>learn ci-cd</h1>
+        <a
+            className="App-link"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
       <h1>value = {state}</h1>
       <button onClick={increment}>
         increment
